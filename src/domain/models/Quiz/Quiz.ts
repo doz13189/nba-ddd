9a4f94d5ce47479785ef4f8aaee7affa66b33387
Takahiro Kato <dozo13189@gmail.com>
2021-08-08 22:15:00 +0900

    README.md にメモを記載

---
+++ src/domain/models/Quiz/Quiz.ts
@@ -0,0 +1,21 @@
+
+// class QuizId {
+//   private quizId: string
+
+//   constructor() {
+
+//   }
+
+//   generateQuizId(): string {
+
+//   }
+
+// }
+
+// class QuizObjectValue {
+//   private 
+// }
+
+export {
+
+}
